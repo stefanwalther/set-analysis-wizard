000be3ddfcb68810cf457ee650d78ae2e778b68a
Stefan Walther <stefanwalther@users.noreply.github.com>
2022-09-25 04:33:53 +0200

    feat: basic app

--- legacy_2/test/global.js
+++
@@ -1,6 +0,0 @@
-global.sinon = require( "sinon" );
-global.chai = require( "chai" );
-global.expect = chai.expect;
-var sinonChai = require( "sinon-chai" );
-
-chai.use( sinonChai );
---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface Props {}
+const Logo: React.FC<Props> = (props) => {
+  return (<></>)
+}
+export default Logo;

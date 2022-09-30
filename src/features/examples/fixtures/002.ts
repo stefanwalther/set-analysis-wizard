501672c8a919d4cec9cafb9713d844759b4fe1b3
Stefan Walther <stefanwalther@users.noreply.github.com>
2022-09-30 22:57:21 +0200

    refactor: re-format example 002

--- src/features/examples/fixtures/002.ts
+++ src/features/examples/fixtures/002.ts
@@ -1,9 +1,13 @@
-{
+import {ISetAnalysisDefinitionProps} from "../../wizard/interfaces/ISetAnalysisDefinitionProps";
+
+const item: ISetAnalysisDefinitionProps = {
   "SetIdentifier": "$",
   "AggregationType": "Count",
   "FieldExpression": "Customer",
   "Bookmark": "",
   "PersonalComment": "Count values of field \"Customer\" based on the current selection",
   "Expression": "Count({$}Customer)",
-  "SetModifierActions": null
+  "SetModifierActions": undefined
 }
+
+export default item;

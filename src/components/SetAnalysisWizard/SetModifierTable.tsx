798c85525dc8642b495f4a314c1977f05c448111
Stefan Walther <stefanwalther@users.noreply.github.com>
2022-10-08 19:55:51 +0200

    feat: EOF message for SetModifier-table

--- src/components/SetAnalysisWizard/SetModifierTable.tsx
+++ src/components/SetAnalysisWizard/SetModifierTable.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {ISetModifier} from "../../common/interfaces";
-import {ActionIcon, ScrollArea, Table} from "@mantine/core";
+import {ActionIcon, ScrollArea, Table, Box} from "@mantine/core";
 import {IconAlphabetLatin, IconPencil, IconTrash} from "@tabler/icons";
 import {setFormState} from "../../features/set-modifier-form/setModifierFormSlice";
 import {deleteModifier, setModifierModalVisibility} from "../../features/wizard/wizardSlice";
 import {useAppDispatch} from "../../common/hooks";
+import {IconCodePlus} from '@tabler/icons';
 
 interface Props {
   modifiers: ISetModifier[];
@@ -32,14 +33,41 @@ const SetModifierTable: React.FC<Props> = ({modifiers}: Props) => {
       </td>
       <td style={{width: 20}}><ActionIcon variant="default" title='Highlight modifier'><IconAlphabetLatin
         size={16}/></ActionIcon></td>
-      <td style={{width: 20}}><ActionIcon variant="default" title='Edit modifier'
-                                          onClick={() => handleClickEdit(sm)}><IconPencil size={16}/></ActionIcon></td>
-      <td style={{width: 20}}><ActionIcon variant="default" title='Delete modifier'
-                                          onClick={() => handleClickDelete(index)}><IconTrash size={16}/></ActionIcon>
+      <td style={{width: 20}}>
+        <ActionIcon
+          variant="default"
+          title='Edit modifier'
+          onClick={() => handleClickEdit(sm)}>
+          <IconPencil size={16}/>
+        </ActionIcon>
+      </td>
+      <td style={{width: 20}}>
+        <ActionIcon
+          variant="default"
+          title='Delete modifier'
+          onClick={() => handleClickDelete(index)}>
+          <IconTrash size={16}/>
+        </ActionIcon>
       </td>
       <td style={{width: 20}}>&nbsp;</td>
     </tr>
   ));
+
+  const rowsEmpty = () => (
+    <tr>
+      <td colSpan={5} style={{height: '260px'}}>
+        <Box
+          sx={(theme) => ({
+            textAlign: 'center',
+            color: '#ccc'
+          })}
+        >
+          <IconCodePlus size={64} style={{color: '#ccc'}}/><br/>
+          No Set Modifiers defined yet
+        </Box>
+      </td>
+    </tr>
+  );
 
   return (
     <ScrollArea type='auto' style={{
@@ -51,12 +79,14 @@ const SetModifierTable: React.FC<Props> = ({modifiers}: Props) => {
     }}>
       <Table striped highlightOnHover>
         <thead>
-        <tr>
+        <tr style={{height: '20px'}}>
           <th>Set Modifier</th>
           <th colSpan={4} style={{textAlign: 'center'}}>Actions</th>
         </tr>
         </thead>
-        <tbody>{rows}</tbody>
+        <tbody>
+        {(modifiers && modifiers.length) > 0 ? rows : rowsEmpty()}
+        </tbody>
       </Table>
     </ScrollArea>
   );
